6c8a23804f96339f27d49bb38b3687e539003283
cosleyw <willcosley@gmail.com>
2023-02-11 12:18:13 -0600

    implimented sqrtf32

--- src/log.c
+++ src/log.c
@@ -41,3 +41,29 @@ i32log2f32(float n)
 {
 	return ((f32asu32(n)>>23) & 0xff) - 127;
 }
+
+/*
+	computes log2 of a float
+
+	it appears to be off by one ulp sometimes, could probably fix my using double instead of floats internally.
+
+	works on anything that isn't nan, subnormal, negative, or infinity
+*/
+float
+log2f32(float n)
+{
+	uint8_t bit = 24;
+	int64_t res = i16log2f32(n)<<24;
+	n = u32asf32((f32asu32(n) & 0x7fffff) | 0x3f800000);
+
+	while(bit--){
+		n*=n;
+		if(f32asu32(n) & 0x40000000){
+			res |= 1ull << bit;
+			n = u32asf32(f32asu32(n) - 0x800000);
+		}
+	}
+
+	return res * 0x1p-24;
+}
+
