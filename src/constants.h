72c3b3b7be0b5682a39957a50582335ab320849b
cosleyw <willcosley@gmail.com>
2023-02-18 18:49:12 -0600

    added stubs and constants for trig

---
+++ src/constants.h
@@ -0,0 +1,11 @@
+#ifndef CMATH_CONSTANTS_H
+#define CMATH_CONSTANTS_H
+
+/*
+	math constants
+*/
+
+#define PI 0xc.90fdaa22168cp-2
+#define E 0xa.df85458a2bb48p-2
+
+#endif
---
+++ src/trig.c
@@ -0,0 +1,14 @@
+#include"common.h"
+#include"constants.h"
+
+float
+cosf32(float n)
+{
+
+}
+
+float
+sinf32(float n)
+{
+
+}
