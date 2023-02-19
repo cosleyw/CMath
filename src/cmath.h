9d7043eb32ab15600301f0bef96b219169afa7c0
cosleyw <willcosley@gmail.com>
2023-02-18 18:25:31 -0600

    moved inline functions to common.h

--- src/cmath.h
+++ src/cmath.h
@@ -7,65 +7,12 @@ uint32_t sqrtu32(uint32_t);
 float sqrtf32(float);
 
 uint32_t expu32(uint32_t, uint32_t);
+float exp2f32(float);
 
 uint32_t log2u32(uint32_t);
-uint8_t ilog2f32(float);
+uint8_t log2f32(float);
 
-/*
-	converts uint32_t to float
-*/
-inline float
-u32asf32(uint32_t a)
-{
-	union{
-		float f;
-		uint32_t i;
-	} conv = {.i = a};
-
-	return conv.f;
-}
-
-/*
-	converts uint64_t to double
-*/
-inline double
-u64asf64(uint64_t a)
-{
-	union{
-		double f;
-		uint64_t i;
-	} conv = {.i = a};
-
-	return conv.f;
-}
-
-/*
-	converts float to uint32_t
-*/
-inline uint32_t
-f32asu32(float a)
-{
-	union{
-		float f;
-		uint32_t i;
-	} conv = {.f = a};
-
-	return conv.i;
-}
-
-/*
-	converts double to uint64_t
-*/
-inline uint64_t
-f64asu64(double a)
-{
-	union{
-		double f;
-		uint64_t i;
-	} conv = {.f = a};
-
-	return conv.i;
-}
-
+float roundf32(float);
+double roundf64(double);
 
 #endif
--- src/common.h
+++ src/common.h
@@ -1,19 +1,57 @@
+#ifndef CMATH_COMMON_H
+#define CMATH_COMMON_H
 
 /*
 
-	this header will define common types and testing macros
+	this header will define common types and functions
 
 */
 
-
-#ifndef CMATH_COMMON_H
-#define CMATH_COMMON_H
 
 //NULL, size_t
 #include<stddef.h>
 //uint_t/int_t types
 #include<stdint.h>
 
+/*
+	converts uint32_t to float
+*/
+inline float
+u32asf32(uint32_t a)
+{
+	union _{float f; uint32_t i;};
+	return ((union _*)&a)->f;
+}
+
+/*
+	converts uint64_t to double
+*/
+inline double
+u64asf64(uint64_t a)
+{
+	union _{double f; uint64_t i;};
+	return ((union _*)&a)->f;
+}
+
+/*
+	converts float to uint32_t
+*/
+inline uint32_t
+f32asu32(float a)
+{
+	union _{float f; uint32_t i;};
+	return ((union _*)&a)->i;
+}
+
+/*
+	converts double to uint64_t
+*/
+inline uint64_t
+f64asu64(double a)
+{
+	union _{double f; uint64_t i;};
+	return ((union _*)&a)->i;
+}
 
 #endif
 
